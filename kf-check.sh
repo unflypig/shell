bf6ababc6a36e3fb71c8a96157d6d0c9eb2f7f15
loulei <474179849@qq.com>
2016-12-22 15:39:41 +0800

    优化：将连接数改为左对齐

--- kf-check.sh
+++ kf-check.sh
@@ -11,12 +11,13 @@ elif [ $cnts -gt 1 ];then
 	echo "too many args!!"
 	exit
 fi
-while true ; do                                                                                                                                                                  
+while true ; do
     cur_time=`date "+%y-%m-%d %H:%M:%S"`                                                                                 
     connect_num_all=`cat /proc/net/nf_conntrack|wc -l`                                                                   
     connect_num_es=`cat /proc/net/nf_conntrack|grep ES|wc -l`                                                            
-    echo -e "[${cur_time}] connect num all: ${connect_num_all}" >> $LOG_FILE_PATH                                        
-    echo -e "[${cur_time}] connect num es: ${connect_num_es}" >> $LOG_FILE_PATH                                          
+    #echo -e "[${cur_time}] connect num all: ${connect_num_all}" >> $LOG_FILE_PATH                                        
+    #echo -e "[${cur_time}] connect num es: ${connect_num_es}" >> $LOG_FILE_PATH                                          
+    printf "[${cur_time}] connect num all: %-10d connect num es: %-10d\n" "${connect_num_all}" "${connect_num_es}" >> $LOG_FILE_PATH
 	lanIp=`uci get network.lan.ipaddr | grep -oE '([0-9]{1,3}\.){2}[0-9]{1,3}'`
     clients_num_all=`cat /proc/net/arp | grep $lanIp | grep 0x2|wc -l`                                              
 	if  [ $apMac = "no" ];then
@@ -24,7 +25,7 @@ while true ; do
 	else
         clients_num_ap=`cat /proc/net/arp | grep $lanIp | grep 0x2  | grep $apMac |wc -l`                                  
 	fi
-    printf "[${cur_time}] client num: all %10s          ap:%10s\n" "$clients_num_all" "$clients_num_ap" >> $LOG_FILE_PATH
+    printf "[${cur_time}] client num all:%-10s          ap:%-10s\n" "$clients_num_all" "$clients_num_ap" >> $LOG_FILE_PATH
     url="114.114.114.114"                                                                                                
     ping $url -c 2 -w 2 > /dev/null                                                                                      
     if [ $? = 0 ];then                                                                                                   
@@ -40,7 +41,7 @@ while true ; do
         echo -e "[${cur_time}] ping $url fail" >> $LOG_FILE_PATH   
     fi                                                             
     #gateway
-    url=`ubus call network.interface.wan status | grep nexthop | grep -oE '([0-9]{1,3}.){3}.[0-9]{1,3}'`                                                                                      
+    url=`ubus call network.interface.wan status | grep nexthop | grep -oE '([1-9]{1,3}.){3}[0-9]{1,3}'`                                                                                      
     ping $url -c 2 -w 2 > /dev/null                                                                             
     if [ $? = 0 ];then                                                                                                       
         echo -e "[${cur_time}] ping $url success" >> $LOG_FILE_PATH                                                          
@@ -49,7 +50,7 @@ while true ; do
     fi                                                                                                          
     echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ifconfig info<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> $LOG_FILE_PATH             
 	line=`ifconfig | grep "eth0.2" -n | grep -oE '^[0-9]{1,3}'`
-    echo -e "`ifconfig | head -n \`expr $line + 6\` | tail -n 7`" >> $LOG_FILE_PATH                                                           
+    echo -e "`ifconfig | head -n \`expr $line + 8\` | tail -n 10`" >> $LOG_FILE_PATH                                                           
     echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>ps info<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> $LOG_FILE_PATH             
     ps www >>  $LOG_FILE_PATH                                                                                             
     echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>top info<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> $LOG_FILE_PATH             
@@ -58,5 +59,5 @@ while true ; do
     cat /proc/cpuinfo >> $LOG_FILE_PATH                                                                                      
     echo -e ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>memery  info<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" >> $LOG_FILE_PATH              
     cat /proc/meminfo >> $LOG_FILE_PATH                                                                                      
-    sleep 180 
+    sleep 5 
 done                             
