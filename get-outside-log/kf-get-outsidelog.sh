0af25fa53b926693a2dce3025980bfad366d0b47
loulei <474179849@qq.com>
2016-12-27 11:50:02 +0800

    杀不死

--- get-outside-log/kf-get-outsidelog.sh
+++ get-outside-log/kf-get-outsidelog.sh
@@ -2,7 +2,8 @@
 #Date:2016年12月26日16:01:45
 #Function:This script is used to get outsidelog from alllog
 #!/bin/bash
-set -x
+#set -x
+LOG_FILE_PATH="/$WORK_DIR/shell/get-outside-log/result.log"
 KF3_MAC_LIST_FILE_PATH="/root/files/for-vip/"
 WORK_DIR="/root/files/for-vip"
 LOG_DIR="/root/kflogs"
@@ -30,21 +31,29 @@ kf_get_outside_network_log(){
 	rm -f $LOG_DIR/$mac/latest-outside-check*
 	[ -f $LOG_DIR/$mac/$logFileName ] && cat $LOG_DIR/$mac/$logFileName | grep -E '\[www' > $LOG_DIR/$mac/latest-outside-check-$currentTime.log
 }
-for mac in $KF3_MAC_LIST;do
-   currentLogFileName=`kf_get_last_log_file_name $mac` 
-   latestLogFileName=`kf_get_latest_log_file_name $mac`
-   if [ $currentLogFileName != $latestLogFileName ];then
-	   #latest log has changed
-	   cat $LAST_LOG_FILE_NAME_FILE_PATH | grep $mac > /dev/null
-	   if [ $? = 0 ];then
-		   #mac alreaady exist 
-		   sed -i "/$mac/d"  $LAST_LOG_FILE_NAME_FILE_PATH
-		   echo -e "$mac $latestLogFileName" >> $LAST_LOG_FILE_NAME_FILE_PATH
-		   kf_get_outside_network_log $latestLogFileName $mac
-	   else
-	   #mac not exist
-		   echo -e "$mac $latestLogFileName" >> $LAST_LOG_FILE_NAME_FILE_PATH
-		   kf_get_outside_network_log $latestLogFileName $mac
-	   fi
-   fi
+while true;do
+    for mac in $KF3_MAC_LIST;do
+    	echo "" > $LOG_FILE_PATH
+        echo "start to deal with $mac" >> $LOG_FILE_PATH 
+        currentLogFileName=`kf_get_last_log_file_name $mac` 
+        latestLogFileName=`kf_get_latest_log_file_name $mac`
+        if [ $currentLogFileName != $latestLogFileName ];then
+    		echo "latest log file hase changed,start to refresh" >> LOG_FILE_PATH
+            #latest log has changed
+            cat $LAST_LOG_FILE_NAME_FILE_PATH | grep $mac > /dev/null
+            if [ $? = 0 ];then
+         	   #mac alreaady exist 
+         	   sed -i "/$mac/d"  $LAST_LOG_FILE_NAME_FILE_PATH
+         	   echo -e "$mac $latestLogFileName" >> $LAST_LOG_FILE_NAME_FILE_PATH
+         	   kf_get_outside_network_log $latestLogFileName $mac
+            else
+            #mac not exist
+         	   echo -e "$mac $latestLogFileName" >> $LAST_LOG_FILE_NAME_FILE_PATH
+         	   kf_get_outside_network_log $latestLogFileName $mac
+            fi
+    	else
+    		echo "latest log file hase  not changed, do nothing" >> $LOG_FILE_PATH
+        fi
+	done
+    sleep 100
 done
