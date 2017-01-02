1c117ffbe845a5405ba7790cf5550e5a488b7f3d
loulei <474179849@qq.com>
2017-01-02 16:25:31 +0800

    增加：自动替换不可用服务器脚本

--- kf-remove-disable-server.sh
+++ kf-remove-disable-server.sh
@@ -3,16 +3,14 @@
 #Function:This script is used to get if this server is useable by ping
 #!/bin/bash
 #set -x
-WORK_DIR="/root/files/for-vip"
+WORK_DIR="/root/files/for-vip/test"
 KF3_MAC_LIST=`ls -lh $WORK_DIR | grep -oE '[0-9]{1,2}M_vip_\w{12}_\w+' \
     | grep -oE '[0-9A-Za-z]{12}'`
-
-
 #this function will get if server is enable or not 
 #input args is baund width
 kf_get_server_enable_and_disable_list_via_bw(){
-	local baundwidth=$1
-    server_list=`cat ${WORK_DIR}/${baundwidth}m_vps.list`
+	local bandwidth=$1
+    server_list=`cat ${WORK_DIR}/${bandwidth}m_vps.list`
     for server_ip in $server_list 
     do
     	#echo "start to deal with $server_ip"
@@ -62,10 +60,10 @@ kf_get_conf_info_via_mac(){
 #get server
 #This function is used to get all server use detail include witch port be used and how many port be used 
 kf_get_server_use_info_via_bw(){
-	local baundwidth=$1
+	local bandwidth=$1
 	local all_kf3_use_detal=`kf_get_conf_info_via_mac`
-	local server_list=`cat ${WORK_DIR}/${baundwidth}m_vps.list`
-    local SERVER_USE_DETAIL_CONF_FILE_PATH="$WORK_DIR/shell/${baundwidth}m_server_use_detail"
+	local server_list=`cat ${WORK_DIR}/${bandwidth}m_vps.list`
+    local SERVER_USE_DETAIL_CONF_FILE_PATH="$WORK_DIR/shell/${bandwidth}m_server_use_detail"
 	rm -f $SERVER_USE_DETAIL_CONF_FILE_PATH
 	for server_ip in $server_list 
 	do
@@ -81,8 +79,8 @@ kf_get_server_use_info_via_bw(){
 	echo -e "`cat $SERVER_USE_DETAIL_CONF_FILE_PATH`"
 }
 kf_get_server_disable_list_via_bw(){
-    local baundwidth=$1	
-	local disable_and_enable_ip_list=`kf_get_server_enable_and_disable_list_via_bw $baundwidth`
+    local bandwidth=$1	
+	local disable_and_enable_ip_list=`kf_get_server_enable_and_disable_list_via_bw $bandwidth`
 	local disable_ip_list=`echo -e "$disable_and_enable_ip_list"\
 	    | grep -oE ".+:0"| grep -oE "^([0-9]{1,3}\.){3}[0-9]{1,3}"`
 	echo -e "$disable_ip_list"
@@ -90,20 +88,24 @@ kf_get_server_disable_list_via_bw(){
 #Tihs function is used to get the best server
 kf_get_best_server_via_bw(){
 	local ip_port_nums_file_tmp="./ip_port_nums_file"
-    local baundwidth=$1    
-	local server_used_detail=`kf_get_server_use_info_via_bw $baundwidth`
+    local bandwidth=$1    
+	local server_used_detail=`kf_get_server_use_info_via_bw $bandwidth`
 	local server_ip_list=`echo -e "$server_used_detail" \
 	    | grep -oE "^([0-9]{1,3}\.){3}[0-9]{1,3}:" | sed "s/://g"`
 	local ip_port_nums=""
 	for ip in $server_ip_list
 	do
-		local ip_port_nums="$ip:`echo -e \"$server_used_detail\"\
-		    | grep $ip |grep -oE \"[0-9]{4,5}:\" | wc -l`"
-		echo -e "$ip_port_nums" >> $ip_port_nums_file_tmp
+		ping $ip -c 1 -w 2 > /dev/null 
+		if [ $? = 0 ];then
+		    local ip_port_nums="$ip:`echo -e \"$server_used_detail\"\
+		        | grep $ip |grep -oE \"[0-9]{4,5}:\" | wc -l`"
+		    echo -e "$ip_port_nums" >> $ip_port_nums_file_tmp
+		fi
 	done
 	local ip_port_nums_all=`cat $ip_port_nums_file_tmp`
 	rm -f $ip_port_nums_file_tmp
-	local best_ip=`echo -e "$ip_port_nums_all" |sort -n -t : -k 2 | head -n 1`
+    #we need at least 4 ip,in case one of ip has already be used in conf file
+	local best_ip=`echo -e "$ip_port_nums_all" |sort -n -t : -k 2 | head -n 4`
 	echo -e "$best_ip"
 }
 #This function is used to get which port has been used via ip 
@@ -159,7 +161,7 @@ kf_get_passwd_via_port(){
 kf_change_disable_server(){
     local mac=$1;local origin_ip=$2;local new_port=$3
 	local new_ip=$4;local new_passwd=$5
-	local conf_file_path="$WORK_DIR/test/$mac"
+	local conf_file_path="$WORK_DIR/$mac"
 	local confLines=`cat $conf_file_path | wc -l`
 	local confLines=`expr $confLines / 8`
 	local head=8;local end=8
@@ -185,25 +187,74 @@ kf_change_disable_server(){
 		head=`expr $head + 8`
 	done
 	echo -e "`cat $conf_content_all_file`"
+    cat $conf_content_all_file > $conf_file_path
 	rm -f $tmp_file
 	rm -rf $conf_content_all_file
 }
 kf_main(){
 	local mac_use_server_detail=`kf_get_conf_info_via_mac`
+	echo -e "################################mac use server detail#####################"
+	echo -e "$mac_use_server_detail"
+	echo -e "##########################################################################"
 	for bw in  5 10 20 
 	do
         #step 1:get disable server ip list
 	    local disable_server_ip_list=`kf_get_server_disable_list_via_bw $bw`
+		if [ -z $disable_server_ip_list ];then
+		   echo -e "INFO:No Disable IP be Used, Stop!"	
+		   exit
+	    fi
+	    echo -e "############################disable ip list ###########################"
+        echo -e "$disable_server_ip_list"		
+	    echo -e "#######################################################################"
 		for ip in $disable_server_ip_list 
 		do
+			local disable_ip=$ip
+			echo -e "INFO:Start to Deal With Disable IP:[$disable_ip]"
+			#step 2:get mac list who has used disable ip
 			local mac_who_use_disable_ip_list=`echo -e "$mac_use_server_detail" \
-			    | grep $ip|grep -oE "^[0-9A-Za-z]{12}"`
-            echo -e "$mac_who_use_disable_ip_list"
-			for mac in $mac_who_use_disable_ip_list
-			do
-			    local ss_conf_file="$WORK_DIR/$mac"
-
-			done
+			    | grep $disable_ip|grep -oE "^[0-9A-Za-z]{12}"`
+			if [  $mac_who_use_disable_ip_list ];then
+			    echo -e "#######################use disable ip mac list#####################"
+                echo -e "$mac_who_use_disable_ip_list"
+			    echo -e "###################################################################"
+			    for mac in $mac_who_use_disable_ip_list
+			    do
+			    	echo -e "INFO:Start to Deal whith MAC:[$mac]"
+                    #step 3:get baundwhith via mac
+                    local bandwidth=`kf_get_baundwhith_via_mac $mac`
+			    	echo -e "INFO:The Bandwidth of $mac is $bandwidth"
+			    	local conf_file="$WORK_DIR/$mac"
+			    	if [ -z $bandwidth ];then
+			    		echo "ERROR:Get Baundwhith Via MAC Fail [$mac]"
+			    		exit
+			    	fi
+                    #step 4:get best server via bandwidth
+			    	local best_server_list=`kf_get_best_server_via_bw $bandwidth`
+			    	echo -e "INFO:Best Server List:\n$best_server_list"
+                    #step 5:check if which ip is not use in this conf file 
+                    for ip in $best_server_list
+			    	do
+			    		local ip_format=`echo -e $ip \
+			    		    | grep -oE "([0-9]{1,3}\.){3}[0-9]{1,3}"`
+			    		echo -e "INFO:Start to try $ip_format"
+			    		cat $conf_file | grep $ip_format > /dev/null
+                        #this ip has not used in this conf file .we can go on ,or,we should use next ip 
+			    		if [ $? != 0 ];then
+                           local best_port=`kf_get_best_port_via_ip $ip_format` 
+			    		   local passwd=`kf_get_passwd_via_port $best_port`
+                           kf_change_disable_server $mac $disable_ip $best_port $ip_format $passwd > /dev/null
+			    		   echo "INFO:Remove Disable IP:$ip_format from $mac Config File Success!"
+			    		   echo "INFO:New Config File is:"
+			    		   echo -e "`cat $conf_file`"
+			    		   exit
+			    		fi
+			    		echo -e "WARNING:$ip_format Has Already be Used in Config File,Try Next"
+			    	done
+			    done
+			else
+				echo -e "INFO:No MAC Use Disable ip [$disable_ip]"
+			fi
 		done
 	done
 }
@@ -214,4 +265,5 @@ kf_main(){
 #kf_get_best_port_via_ip $1
 #kf_get_baundwhith_via_mac $1
 #kf_get_passwd_via_port $1
-kf_change_disable_server $1 $2 $3 $4 $5
+#kf_change_disable_server $1 $2 $3 $4 $5
+kf_main
