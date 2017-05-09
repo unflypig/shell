57639dca2420852e7747db38b7d792fa076bc702
unflypig <1085025884@qq.com>
2017-05-09 13:50:47 +0800

    优化：增加对IP地址合法性检测

--- kf-add-publicip-into-ipset.sh
+++ kf-add-publicip-into-ipset.sh
@@ -2,11 +2,25 @@
 #Data:2017年4月17日14:04:34
 #Function:This scrip is used to add public ip into ipset list
 #!/bin/sh
+GLOBAL_MODE="3"
 vpn_mode="`uci get /etc/kfconfig/kfconfig.vpn.mode`"
-if [ "$vpn_mode" = "3" ];then
+#check if ipv4 addr  valid
+ipv4valid(){
+    local ip=$1
+    echo -e "$ip" |grep -oE "^([0-9]{1,3}\.){3}[0-9]{1,3}$" -q
+    if [ $? = 0 ];then
+        echo "true"
+    else
+        echo "flase"
+    fi
+}
+if [ "$vpn_mode" = "$GLOBAL_MODE" ];then
     public_ip="`curl http://checkip.amazonaws.com/`"
-    ipset list inlist|grep $public_ip -q
-    if [ "$?" != 0 ];then
-        ipset add inlist $public_ip
+    public_ip_valid="`ipv4valid $public_ip`"
+    if [ "$public_ip_valid" == "true" ];then
+        ipset list inlist|grep $public_ip -q
+        if [ "$?" != 0 ];then
+            ipset add inlist $public_ip
+        fi
     fi
 fi
